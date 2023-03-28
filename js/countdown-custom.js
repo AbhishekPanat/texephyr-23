9805e60c37f04622735fb16a9c0feda2e76183c4
AbhishekPanat <abhipanat676@gmail.com>
2023-03-28 15:50:33 +0530

    First Version

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 3, 22, 0, 0, 0)}); // year, month, date, hour
+        });
+        //console.log(Date(2022, 3, 20, 8));
+});		
+
